9541199879de4e829c1c172f784a14632fb8123f
lucaskatata <104213451+lucaskatata@users.noreply.github.com>
2025-03-04 11:36:42 -0300

    atualizações

--- app.py
+++ app.py
@@ -1,3 +1,13 @@
 # %%
 import streamlit as st
 import pandas as pd
+
+st.set_page_config(layout='wide')
+st.title('Controle da Produção 2024')
+
+# %%
+dataset = 'datasets/CONTROLE DA PRODUCAO E M.O. - 2024.csv'
+
+df = pd.read_csv(dataset)
+
+st.dataframe(df)
